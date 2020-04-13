f7227bbb8d756e5abce0ca1d7bf31a76dfe6e31f
MufasaDoodle <christian_sejer@hotmail.com>
2020-04-13 17:45:02 +0200

    Implemented temporary satellite and money generation

--- src/pulsar/views/mainview/MainController.java
+++ src/pulsar/views/mainview/MainController.java
@@ -1,13 +1,36 @@
 package pulsar.views.mainview;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import pulsar.core.ViewHandler;
 import pulsar.core.ViewModelFactory;
 import pulsar.views.ViewController;
 
 public class MainController implements ViewController
 {
+  @FXML private Label errorLabel;
+  @FXML private Label moneyAmount;
+  @FXML private Label satAmount;
+  private MainViewModel viewModel;
+  private ViewHandler vh;
+
   @Override public void init(ViewHandler viewHandler, ViewModelFactory vmf)
   {
+    this.viewModel = vmf.getMainViewModel();
+    this.vh = viewHandler;
+    moneyAmount.textProperty().bind(viewModel.moneyProperty());
+    satAmount.textProperty().bind(viewModel.satellitesProperty());
+    errorLabel.textProperty().bind(viewModel.errorProperty());
+  }
 
+  public void onLaunchSat(ActionEvent actionEvent)
+  {
+    viewModel.launchSatellite();
+  }
+
+  public void onAskMoney(ActionEvent actionEvent)
+  {
+    viewModel.askForMoney();
   }
 }
--- src/pulsar/views/mainview/MainViewModel.java
+++ src/pulsar/views/mainview/MainViewModel.java
@@ -1,13 +1,94 @@
 package pulsar.views.mainview;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.util.Duration;
 import pulsar.model.Model;
 
 public class MainViewModel
 {
   private Model model;
+  private StringProperty money;
+  private StringProperty satellites;
+  private StringProperty error;
 
   public MainViewModel(Model model)
   {
     this.model = model;
+    money = new SimpleStringProperty();
+    satellites = new SimpleStringProperty();
+    error = new SimpleStringProperty();
+    money.set("1000");
+    satellites.set("0");
+  }
+
+  public void askForMoney()
+  {
+    incrementProperty(money, 500);
+  }
+
+  public void launchSatellite()
+  {
+    makePurchase(satellites, 1000, 1);
+  }
+
+  private void makePurchase(StringProperty property, int cost, int amount)
+  {
+    if (checkPurchase(cost))
+    {
+      incrementProperty(property, amount);
+      decrementProperty(money, cost);
+    }
+    else
+    {
+      error.set("Not enough money");
+      Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3), event -> error.set("")));
+      timeline.play();
+    }
+  }
+
+  private boolean checkPurchase(int cost)
+  {
+    if (Integer.parseInt(money.getValue()) >= cost)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  private void incrementProperty(StringProperty property, int amount)
+  {
+    int temp = Integer.parseInt(property.getValue());
+    temp += amount;
+    property.set(String.valueOf(temp));
+  }
+
+  private void decrementProperty(StringProperty property, int amount)
+  {
+    int temp = Integer.parseInt(property.getValue());
+    temp -= amount;
+    property.set(String.valueOf(temp));
+  }
+
+  public StringProperty moneyProperty()
+  {
+    return money;
+  }
+
+  public StringProperty satellitesProperty()
+  {
+    return satellites;
+  }
+
+  public StringProperty errorProperty()
+  {
+    return error;
   }
 }
